41b9ecd50c917574cb6476298bc3058f281852b7
yayyzea <azkiyaz65@gmail.com>
2026-02-20 22:52:51 +0700

    Update Employee.java

--- W2_P_1D_124/2 - Interaksi Object dan Struktur Class dalam Java/Employee.java
+++ W2_P_1D_124/2 - Interaksi Object dan Struktur Class dalam Java/Employee.java
@@ -1,1 +1,64 @@
+package id.ac.polban.employee.model;
 
+/**
+ *
+ * @author ZAHRA
+ */
+public class Employee {
+    private int id;
+    private String name;
+    private Department department;     
+    private EmploymentType type;       
+    private double salary;
+
+    // static field - menghitung total karyawan 
+    private static int totalEmployees = 0;
+
+    // constructor
+    public Employee(int id, String name, Department department, EmploymentType type, double salary) {
+        this.id         = id;
+        this.name       = name;
+        this.department = department;
+        this.type       = type;
+        this.salary     = salary;
+
+        totalEmployees++; 
+    }
+
+    // static method
+    public static int getTotalEmployees() {
+        return totalEmployees;
+    }
+
+    // getter dan Setter
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Department getDepartment() {
+        return department;
+    }
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+    public EmploymentType getType() {
+        return type;
+    }
+    public void setType(EmploymentType type) {
+        this.type = type;
+    }
+    public double getSalary() {
+        return salary;
+    }
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
